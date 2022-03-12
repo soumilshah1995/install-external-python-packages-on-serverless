b6a9a9c2c5061bc70e45602426793e4af98975d6
soumilshah1995 <soushah@my.bridgeport.edu>
2022-03-12 09:44:21 -0500

    added

---
+++ Lab5/learn-sqs/handler.py
@@ -0,0 +1,11 @@
+import json
+
+
+def hello(event, context):
+    print(event)
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
